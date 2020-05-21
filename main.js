8a50251f868c8239aa65379fc99409057848ca2b
Waldo Mendoza <mendozawaldos@hotmail.com>
2020-05-21 13:54:07 -0600

    Agregue funcion al js y cambie id

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
-let compraJuego = document.getElementById('compraJuego')
+// let compraJuego = document.getElementById('compraJuego')
+let carreta = document.getElementById('carreta')
+let boton = document.getElementById('boton')
 
 const canasta = () => {
-    
+    if (boton === 'click'){
+        carreta.innerHTML = 0
+    } else {
+        carreta.innerHTML = 1
+    }
 }
 
+boton.addEventListener('click', canasta)
